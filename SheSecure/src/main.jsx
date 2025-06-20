5f24f6967e4e24783bc3456dc59fdbd22c902bef
kuldeepbhagbole <kuldeepbhagbole69658@gamil.com>
2025-06-20 19:06:35 +0530

    start frontend with React and tailwind

---
+++ SheSecure/src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App=()=>{
+  return (
+    <div>
+      start the frontend
+    </div>
+  )
+}
+export default App;
+
+createRoot(document.getElementById("root")).render(<App/>);
