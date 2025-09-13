31c0cd9b864ba6ac205a03364799c51b44c08004
kuldeepbhagbole <kuldeepbhagbole69658@gamil.com>
2025-09-13 10:15:45 +0530

    updated routes

--- Backend/routes/CrimeReportsInteraction.js
+++ Backend/routes/CrimeReportsInteraction.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import {
-  getCrimeStats,
   getCrimeInteractions,
   interactWithCrime,
   commentOnCrime
@@ -9,7 +8,6 @@ import { authenticateUser, isUser } from '../utils/authenticateUser.js';
 
 const router = express.Router();
 
-router.get('/stats', authenticateUser, isUser, getCrimeStats);
 router.get('/crime-interaction/:crimeId', authenticateUser, isUser, getCrimeInteractions);
 router.post('/:crimeId/interact', authenticateUser, isUser, interactWithCrime);
 router.post('/:crimeId/comment', authenticateUser, isUser, commentOnCrime);
